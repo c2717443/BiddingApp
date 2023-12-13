c734e93e96e974bb359e5fef83cfb8609f2ad46e
tampo077 <hashamrehmat@gmail.com>
2023-12-13 05:06:38 +0000

    update

--- app/src/main/java/com/test/biddingapp/ui/theme/Theme.kt
+++ app/src/main/java/com/test/biddingapp/ui/theme/Theme.kt
@@ -19,7 +19,7 @@ private val LightColorPalette = lightColors(
 
     /* Other default colors to override
     background = Color.White,
-    surface = Color.White,
+    surface = Color.White, 
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
